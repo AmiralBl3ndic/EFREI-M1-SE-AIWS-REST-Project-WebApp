716e7a97d523bd54a6d7a8acc88bb64a5995db09
Camille Briand <camille.briand@efrei.net>
2019-11-30 20:45:06 +0100

    Move "function" components to actual components

--- src/views/AuthView.js
+++ src/views/AuthView.js
@@ -3,16 +3,13 @@ import React from "react";
 import { Card, CardHeader, CardBody, CardTitle, CardFooter, Button } from 'shards-react';
 import Row from "react-bootstrap/Row";
 
-export default class AuthView extends React.Component {
+export default class AuthView extends React.PureComponent {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			createAccount: false
+			isLoggingIn: true
 		};
-
-		this.AuthFormFooter = this.AuthFormFooter.bind(this);
-		this.AuthFormHeader = this.AuthFormHeader.bind(this);
 	}
 
 
@@ -20,58 +17,55 @@ export default class AuthView extends React.Component {
 		return (
 			<Row className="vh-100 align-middle bg-light" noGutters={true}>
 				<Card className="col-10 offset-1 col-md-8 offset-md-2 align-self-center">
-					{ this.AuthFormHeader() }
+					<AuthFormHeader isLoggingIn={this.state.isLoggingIn} />
+
 					<CardBody>
 
-
 					</CardBody>
-					{ this.AuthFormFooter() }
+
+					<AuthFormFooter isLoggingIn={this.state.isLoggingIn} onClick={() => this.setState({isLoggingIn: !this.state.isLoggingIn})} />
 				</Card>
 			</Row>
 		);
 	}
+}
 
-	/**
-	 * Renders the header of the authentication form accordingly to state
-	 */
-	AuthFormHeader() {
+/**
+ * Renders the header of the authentication form accordingly to state
+ */
+class AuthFormHeader extends React.PureComponent {
+	render() {
 		return (
 			<CardHeader>
 				<CardTitle className="mb-0">
-					{ this.state.createAccount ? "Create an account" : "Login" }
+					{ this.props.isLoggingIn ? "Login" : "Create an account" }
 				</CardTitle>
 			</CardHeader>
 		);
 	}
+}
 
-	/**
-	 * Renders the footer of the authentication form accordingly to state
-	 */
-	AuthFormFooter() {
-		const handleClick = () => {
-			this.setState({
-				createAccount: !this.state.createAccount
-			});
-		};
-
-		if (this.state.createAccount) {
+/**
+ * Renders the footer of the authentication form accordingly to state
+ */
+class AuthFormFooter extends React.PureComponent {
+	render() {
+		if (this.props.isLoggingIn) {
 			return (
 				<CardFooter>
-					<span>Already have an account?</span>
+					<span>Do not have an account yet?</span>
 					<br className="d-block d-md-none" />
-					<Button size="sm" className="mt-2 mt-md-0 ml-md-2 px-1 px-md-2" onClick={handleClick}>Login</Button>
+					<Button size="sm" className="mt-2 mt-md-0 ml-md-2 px-1 px-md-2" onClick={this.props.onClick}>Create an account</Button>
 				</CardFooter>
 			);
 		} else {
 			return (
 				<CardFooter>
-					<span>Do not have an account yet?</span>
+					<span>Already have an account?</span>
 					<br className="d-block d-md-none" />
-					<Button size="sm" className="mt-2 mt-md-0 ml-md-2 px-1 px-md-2" onClick={handleClick}>Create an account</Button>
+					<Button size="sm" className="mt-2 mt-md-0 ml-md-2 px-1 px-md-2" onClick={this.props.onClick}>Login</Button>
 				</CardFooter>
 			);
 		}
 	}
 }
-
-
