13fb3994a2df382f5654fd6a42f873a59f09fb9e
Camille Briand <camille.briand@efrei.net>
2019-12-01 00:28:13 +0100

    Fix wrong URIs building

--- src/uris.js
+++ src/uris.js
@@ -1,4 +1,4 @@
-const apiUri = "http://localhost:8080/AIWS_REST_Project_war_exploded/api/";
+const apiUri = "http://localhost:8080/AIWS_REST_Project_war_exploded/api";
 
 export const authEndpoint = apiUri + "/auth";
 export const usersEndpoint = apiUri + "/users";
