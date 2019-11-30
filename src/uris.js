efc0b1b6f5eeb603e9da8de15117447f19333ecf
Camille Briand <camille.briand@efrei.net>
2019-11-30 19:16:02 +0100

    Add src/uris.js to keep track of API URIs

---
+++ src/uris.js
@@ -0,0 +1,15 @@
+const apiUri = "http://localhost:8080/AIWS_REST_Project_war_exploded/api/";
+
+export const authEndpoint = apiUri + "/auth";
+export const usersEndpoint = apiUri + "/users";
+export const booksEndpoint = apiUri + "/books";
+export const videoGamesEndpoint = apiUri + "/video-games";
+export const dvdsEndpoint = apiUri + "/dvds";
+
+export default {
+	authEndpoint,
+	usersEndpoint,
+	booksEndpoint,
+	videoGamesEndpoint,
+	dvdsEndpoint
+};
