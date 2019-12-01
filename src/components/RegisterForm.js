04abc759fdb6b2dfea27628a3a650c308e62c6c4
Camille Briand <camille.briand@efrei.net>
2019-12-01 14:53:58 +0100

    Start to use AuthenticationContext on RegisterForm

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -3,6 +3,7 @@ import PropTypes from "proptypes";
 
 import {Button, Form, FormGroup, FormInput, InputGroup, InputGroupAddon, InputGroupText} from "shards-react";
 import Row from "react-bootstrap/Row";
+import {AuthenticationContext} from "../context/AuthenticationProvider";
 
 export default class RegisterForm extends React.PureComponent {
 	constructor(props) {
@@ -78,75 +79,79 @@ export default class RegisterForm extends React.PureComponent {
 
 	render() {
 		return (
-			<Form className="px-md-4" onSubmit={this.props.onSubmit}>
-				<FormGroup>
-					<label htmlFor="email">Email</label>
-					<InputGroup seamless>
-						<InputGroupAddon type="prepend">
-							<InputGroupText>📧</InputGroupText>
-						</InputGroupAddon>
-						<FormInput type="email" id="email" name="email" placeholder="Email" required
-						           defaultValue={this.state.email}
-						           onChange={this.handleEmailChange}
-						           onBlur={this.validateEmail}
-						           invalid={this.state.emailValid !== null && !this.state.emailValid}
-						           valid={this.state.emailValid !== null && this.state.emailValid}
-						/>
-					</InputGroup>
-				</FormGroup>
+			<AuthenticationContext.Consumer>
+				{value => (
+					<Form className="px-md-4" onSubmit={this.props.onSubmit}>
+						<FormGroup>
+							<label htmlFor="email">Email</label>
+							<InputGroup seamless>
+								<InputGroupAddon type="prepend">
+									<InputGroupText>📧</InputGroupText>
+								</InputGroupAddon>
+								<FormInput type="email" id="email" name="email" placeholder="Email" required
+								           defaultValue={this.state.email}
+								           onChange={this.handleEmailChange}
+								           onBlur={this.validateEmail}
+								           invalid={this.state.emailValid !== null && !this.state.emailValid}
+								           valid={this.state.emailValid !== null && this.state.emailValid}
+								/>
+							</InputGroup>
+						</FormGroup>
 
-				<FormGroup>
-					<label htmlFor="password">Password</label>
-					<InputGroup seamless>
-						<InputGroupAddon type="prepend">
-							<InputGroupText>🔐</InputGroupText>
-						</InputGroupAddon>
-						<FormInput type="password" id="password" name="password" placeholder="Password" required
-						           defaultValue={this.props.currentPassword ? this.props.currentPassword : ''}
-						           onChange={this.handlePasswordChange}
-						           onBlur={this.validatePassword}
-						           invalid={this.state.passwordValid !== null && !this.state.passwordValid}
-						           valid={this.state.passwordValid !== null && this.state.passwordValid}
-						/>
-					</InputGroup>
-				</FormGroup>
+						<FormGroup>
+							<label htmlFor="password">Password</label>
+							<InputGroup seamless>
+								<InputGroupAddon type="prepend">
+									<InputGroupText>🔐</InputGroupText>
+								</InputGroupAddon>
+								<FormInput type="password" id="password" name="password" placeholder="Password" required
+								           defaultValue={this.props.currentPassword ? this.props.currentPassword : ''}
+								           onChange={this.handlePasswordChange}
+								           onBlur={this.validatePassword}
+								           invalid={this.state.passwordValid !== null && !this.state.passwordValid}
+								           valid={this.state.passwordValid !== null && this.state.passwordValid}
+								/>
+							</InputGroup>
+						</FormGroup>
 
-				<FormGroup>
-					<label htmlFor="confirm-password">Confirm password</label>
-					<InputGroup seamless>
-						<InputGroupAddon type="prepend">
-							<InputGroupText>🔑</InputGroupText>
-						</InputGroupAddon>
-						<FormInput type="password" id="confirm-password" name="confirm-password" placeholder="Confirm password" required
-						           onChange={this.handleConfirmPasswordChange}
-						           invalid={this.state.passwordsMatch !== null && !this.state.passwordsMatch}
-						           valid={this.state.passwordsMatch !== null && this.state.passwordsMatch}
-						/>
-					</InputGroup>
-				</FormGroup>
+						<FormGroup>
+							<label htmlFor="confirm-password">Confirm password</label>
+							<InputGroup seamless>
+								<InputGroupAddon type="prepend">
+									<InputGroupText>🔑</InputGroupText>
+								</InputGroupAddon>
+								<FormInput type="password" id="confirm-password" name="confirm-password" placeholder="Confirm password" required
+								           onChange={this.handleConfirmPasswordChange}
+								           invalid={this.state.passwordsMatch !== null && !this.state.passwordsMatch}
+								           valid={this.state.passwordsMatch !== null && this.state.passwordsMatch}
+								/>
+							</InputGroup>
+						</FormGroup>
 
-				<FormGroup>
-					<label htmlFor="city">City</label>
-					<InputGroup seamless>
-						<InputGroupAddon type="prepend">
-							<InputGroupText>🏠</InputGroupText>
-						</InputGroupAddon>
-						<FormInput type="text" id="city" name="city" placeholder="city" required
-						           defaultValue={this.props.currentCity ? this.props.currentCity : ''}
-						           onChange={this.props.handleInputChange}
-						/>
-					</InputGroup>
-				</FormGroup>
+						<FormGroup>
+							<label htmlFor="city">City</label>
+							<InputGroup seamless>
+								<InputGroupAddon type="prepend">
+									<InputGroupText>🏠</InputGroupText>
+								</InputGroupAddon>
+								<FormInput type="text" id="city" name="city" placeholder="city" required
+								           defaultValue={this.props.currentCity ? this.props.currentCity : ''}
+								           onChange={this.props.handleInputChange}
+								/>
+							</InputGroup>
+						</FormGroup>
 
-				<Row className="justify-content-center justify-content-md-between mt-4">
-					<Button outline type="reset" size="sm" theme="danger" className="order-2 order-md-1 mt-4 mt-md-0" onClick={() => this.setState({email:'', password:'', city:'', emailValid: null, passwordValid: null, passwordsMatch: null})}>
-						Reset
-					</Button>
-					<Button type="submit" className="col-12 col-md-4 order-1 order-md-2">
-						Create an account
-					</Button>
-				</Row>
-			</Form>
+						<Row className="justify-content-center justify-content-md-between mt-4">
+							<Button outline type="reset" size="sm" theme="danger" className="order-2 order-md-1 mt-4 mt-md-0" onClick={() => this.setState({email:'', password:'', city:'', emailValid: null, passwordValid: null, passwordsMatch: null})}>
+								Reset
+							</Button>
+							<Button type="submit" className="col-12 col-md-4 order-1 order-md-2">
+								Create an account
+							</Button>
+						</Row>
+					</Form>
+				)}
+			</AuthenticationContext.Consumer>
 		);
 	}
 }
