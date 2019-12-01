97d09e3fdd4fe0ee01a2f632293dfbfb7816c594
Camille Briand <camille.briand@efrei.net>
2019-12-01 17:09:17 +0100

    Add Ant Design CSS stylesheet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from "react-dom";
 
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 import './style.scss';
 
